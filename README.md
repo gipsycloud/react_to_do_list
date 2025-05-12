9315326001b76fc61e585a16ba6e544fb340677d
webgipsy <babybird1801@gmail.com>
2025-05-12 23:54:50 +0630

    key props and spread operator

--- README.md
+++ README.md
@@ -68,3 +68,16 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+- CSS
+- JSX - CSS
+- JSX - Javascript
+- props - properties
+- Props - Destructuring
+- Props - Children
+- Simple List
+- propers List
+- key Prop and Spread Operator
+- Even Basic
+- import and Export Statements
--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const cards = [
+  {
+    id: 1,
+    title: "Card Title",
+    text: "This is a description of the card. It contains some information about the content.",
+    image: "https://imgs.search.brave.com/hEQ58nNRwjSFMXJggjpS1FL09XKOOJCIjhsIQMRIx5Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aHVic3BvdC5jb20v/aHViZnMvSG93JTIw/dG8lMjBTZXQlMjBV/cCUyMGFuJTIwSFRN/TCUyMFJlZGlyZWN0/JTIwb24lMjBZb3Vy/JTIwV2Vic2l0ZS53/ZWJw",
+  },
+  {
+    id: 2,
+    title: "Card Title",
+    text: "This is a description of the card. It contains some information about the content.",
+    image: "https://imgs.search.brave.com/hEQ58nNRwjSFMXJggjpS1FL09XKOOJCIjhsIQMRIx5Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aHVic3BvdC5jb20v/aHViZnMvSG93JTIw/dG8lMjBTZXQlMjBV/cCUyMGFuJTIwSFRN/TCUyMFJlZGlyZWN0/JTIwb24lMjBZb3Vy/JTIwV2Vic2l0ZS53/ZWJw",
+  },
+  {
+    id: 3,
+    title: "Card Title",
+    text: "This is a description of the card. It contains some information about the content.",
+    image: "https://imgs.search.brave.com/hEQ58nNRwjSFMXJggjpS1FL09XKOOJCIjhsIQMRIx5Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aHVic3BvdC5jb20v/aHViZnMvSG93JTIw/dG8lMjBTZXQlMjBV/cCUyMGFuJTIwSFRN/TCUyMFJlZGlyZWN0/JTIwb24lMjBZb3Vy/JTIwV2Vic2l0ZS53/ZWJw",
+  },
+  {
+    id: 4,
+    title: "Card Title",
+    text: "This is a description of the card. It contains some information about the content.",
+    image: "https://imgs.search.brave.com/hEQ58nNRwjSFMXJggjpS1FL09XKOOJCIjhsIQMRIx5Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aHVic3BvdC5jb20v/aHViZnMvSG93JTIw/dG8lMjBTZXQlMjBV/cCUyMGFuJTIwSFRN/TCUyMFJlZGlyZWN0/JTIwb24lMjBZb3Vy/JTIwV2Vic2l0ZS53/ZWJw",
+  },
+  {
+    id: 5,
+    title: "Card Title",
+    text: "This is a description of the card. It contains some information about the content.",
+    image: "https://imgs.search.brave.com/hEQ58nNRwjSFMXJggjpS1FL09XKOOJCIjhsIQMRIx5Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aHVic3BvdC5jb20v/aHViZnMvSG93JTIw/dG8lMjBTZXQlMjBV/cCUyMGFuJTIwSFRN/TCUyMFJlZGlyZWN0/JTIwb24lMjBZb3Vy/JTIwV2Vic2l0ZS53/ZWJw",
+  },
+];
+
+function CardList() {
+  return (
+    <main className="card-list">
+      {cards.map((card) => {
+        return <Card key={card.id} {...card} />;
+      })}
+    </main>
+  );
+}
+
+function Card(props) {
+  console.log(props)
+  // destructuring props
+  const { image, title, text, number } = props;
+  return (
+    <section className="card">
+      <img style={{ width: "50%", height: "50%" }} src={image} alt='Random' />
+      <h2 style={{ marginTop: "1rem" }}>{title}</h2>
+      <p>{text}</p>
+      <p>{number}</p>
+    </section >
+  );
+}
+
+function Description() {
+  return <p style={{ marginTop: "0.5rem" }}>This is a description of the card. It contains some information about the content.</p>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<CardList />);
