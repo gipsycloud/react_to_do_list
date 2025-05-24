3711f9a114b6c4f881d2abdac07830a24d146eba
webgipsy <babybird1801@gmail.com>
2025-05-24 23:46:22 +0630

    use state

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,16 +1,24 @@
 // React Hooks ..
-// useState
-var count = 0;
-function increase() {
-  count++;
-  console.log(count);
-}
+import { useState } from "react";
 
 function Calculator() {
+  const state = useState(10);
+  // array destructuring
+  function increate() {
+    state[1](state[0] + 1);
+  }
+  function decrease() {
+    state[1](state[0] - 1);
+  }
+  function reset() {
+    state[1](10);
+  }
+  // state[0] is the current value
+
   return (
     <div style={{ textAlign: 'center', margin: '2rem', padding: '2rem', border: '1px solid black', borderRadius: '5px' }}>
-      <h1>Calculator</h1>
-      <button onClick={increase} style={{ padding: '0.5rem', margin: '0.5rem', color: 'green' }}>+</button>
+      <h1>{state[0]}</h1>
+      <button onClick={increate} style={{ padding: '0.5rem', margin: '0.5rem', color: 'green' }}>+</button>
     </div>
   );
 }
