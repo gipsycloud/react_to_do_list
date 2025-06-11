b880b24c14e3b183cb22d64a29f8bf94118977b6
webgipsy <babybird1801@gmail.com>
2025-06-11 11:12:28 +0630

    adding fontawesome icon

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,6 +1,7 @@
 import "./Profile.css"; // Assuming you have a CSS file for styling
 import { profileDatas } from "./profile_data";
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 function ProfileList() {
   return (
     <div className="profile-list">
@@ -16,7 +17,7 @@ function Profile(props) {
   console.log(props);
   return (
     <div className="profile-div">
-      <p></p>
+      <p><FontAwesomeIcon icon={ faCircleUser }/></p>
       <h2>Name: {name} </h2>
       <h2>Age: { age }</h2>
       <h2>Job: { job? job : "109/110" }</h2>
